7407877c59cc3df7b6c213b5df86c9f51e26e55b
Carlos Eduardo <carloseduardodudu558@hotmail.com>
2025-12-16 13:09:58 -0300

    casa-16-12

--- index.php
+++ index.php
@@ -5,21 +5,24 @@ require 'conexao.php';
 
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Usuarios</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-  </head>
-  <body>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
     <?php include('navbar.php'); ?>
     <div class="container mt-4">
-        <?php include('mensagem.php');?>
+        <?php include('mensagem.php'); ?>
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
-                        <h4> Lista de Usuários 
+                        <h4> Lista de Usuários
                             <a href="usuario-create.php" class="btn btn-primary float-end">Adicionar usuário</a>
                         </h4>
                     </div>
@@ -35,21 +38,34 @@ require 'conexao.php';
                                 </tr>
                             </thead>
                             <tbody>
+                                <?php
+                                $sql = 'SELECT * FROM usuarios';
+                                $usuarios = mysqli_query($conexao, $sql);
+                                if (mysqli_num_rows($usuarios) > 0) {
+                                    foreach($usuarios as $usuario) {
+                                ?>
                                 <tr>
-                                    <td>1</td>
-                                    <td>teste</td>
-                                    <td>teste@gmail.com</td>
-                                    <td>01/01/2010</td>
+                                    <td><?=$usuario['id']?></td>
+                                    <td><?=$usuario['nome']?></td>
+                                    <td><?=$usuario['email']?></td>
+                                    <td><?=date('d/m/Y', strtotime($usuario['data_nascimento']))?></td>
                                     <td>
-                                        <a href="" class="btn btn-secondary btn-sm">Visualizar</a>
-                                        <a href="" class="btn btn-success btn-sm">Editar</a>
+                                        <a href="usuario-view.php?id=<?=$usuario['id']?>" class="btn btn-secondary btn-sm">Visualizar</a>
+                                        <a href="usuario-edit.php?id=<?=$usuario['id']?>" class="btn btn-success btn-sm">Editar</a>
                                         <form action="" method="POST" class="d-inline">
-                                            <button type="submit" name="delete_usuario" value="1" class="btn btn-danger btn-sm">
-                                            Excluir
+                                            <button type="submit" name="delete_usuario" value="1"
+                                                class="btn btn-danger btn-sm">
+                                                Excluir
                                             </button>
                                         </form>
                                     </td>
                                 </tr>
+                                <?php
+                                }
+                            } else {
+                                echo '<h5>Nenhum usuario encontrado</h5>';
+                            }
+                                ?>
                             </tbody>
                         </table>
                     </div>
@@ -57,6 +73,9 @@ require 'conexao.php';
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-  </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
+</body>
+
 </html>
